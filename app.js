64493060558deefb2432f099bc126b0e8e2f2002
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-06-23 17:07:28 -0300

    Configurado rotas
    Foram configuradas algumas novas rotas como exemplo para o projeto.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var http = require('http');
+var routes = require('./routes');
 var app = express();
 
 app.get('/', function(req, res) {
     res.json({ routeName: 'Curso de NodeJS com Express'});
 });
+app.use('/hello', routes);
 
 http.createServer(app).listen(3000, function() {
     console.log('Express started!')
