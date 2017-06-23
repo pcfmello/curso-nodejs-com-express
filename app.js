b94f73190f4d49c075c01860f7b987747acbe72d
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-06-23 17:37:42 -0300

    Configurado o body-parser
    Configuração do body-parser para obter os atributos enviados no body da requisição POST(através do form)

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var http = require('http');
 var routes = require('./routes');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.get('/', function(req, res) {
     res.json({ routeName: 'Curso de NodeJS com Express'});
